9da52a1e6f2f6612c017a29bcb12226e921215a0
Leonor Da Cunha Domingues <lda-cunh@c1r15s15.42lisboa.com>
2022-07-07 17:45:35 +0100

    tet

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: lda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:25:10 by lda-cunh          #+#    #+#             */
-/*   Updated: 2022/07/06 16:25:17 by lda-cunh         ###   ########.fr       */
+/*   Updated: 2022/07/07 17:44:38 by lda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,15 @@ void	ft_putchar(char c)
 void	ft_putstr_non_printable(char *str)
 {
 	int	i;
-	char	a;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		
-		
-	}
-}
+		if
+
+		else
+		{
+			ft_putchar ('\\');
+			ft_putchar ("0123456789abcdef"
+			ft_putchar ("0123456789abcdef"
+

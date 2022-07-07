a78fdb91cdad8173c2c90887a7ac3cbfb63cba02
Leonor Da Cunha Domingues <lda-cunh@c1r15s15.42lisboa.com>
2022-07-07 18:09:02 +0100

    11

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: lda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:25:10 by lda-cunh          #+#    #+#             */
-/*   Updated: 2022/07/07 17:44:38 by lda-cunh         ###   ########.fr       */
+/*   Updated: 2022/07/07 18:08:21 by lda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,20 @@ void	ft_putstr_non_printable(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if
-
-		else
+		if (str[i] >= 0 && str[i] <= 31)
 		{
 			ft_putchar ('\\');
-			ft_putchar ("0123456789abcdef"
-			ft_putchar ("0123456789abcdef"
+			ft_putchar ("0123456789abcdef"[str[i] / 16]);
+			ft_putchar ("0123456789abcdef"[str[i] % 16]);
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+}
 
+/*int	main()
+{
+	char	str[] = "what\never";
+	ft_putstr_non_printable(str);
+}*/
